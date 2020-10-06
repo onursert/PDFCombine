7d7edb6a3bfeed6ff797ac7542a9caf5e7279b1f
Onur <43381986+onursert@users.noreply.github.com>
2020-10-06 21:48:19 +0300

    Add files via upload

---
+++ PDFCombine/PDFCombine.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace PDFCombine
+{
+    partial class PDFCombine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add_pdf = new System.Windows.Forms.Button();
+            this.btn_combine = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_add_pdf
+            // 
+            this.btn_add_pdf.Location = new System.Drawing.Point(12, 12);
+            this.btn_add_pdf.Name = "btn_add_pdf";
+            this.btn_add_pdf.Size = new System.Drawing.Size(75, 23);
+            this.btn_add_pdf.TabIndex = 7;
+            this.btn_add_pdf.Text = "Add PDF";
+            this.btn_add_pdf.UseVisualStyleBackColor = true;
+            this.btn_add_pdf.Click += new System.EventHandler(this.btn_add_pdf_Click);
+            // 
+            // btn_combine
+            // 
+            this.btn_combine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_combine.Location = new System.Drawing.Point(397, 277);
+            this.btn_combine.Name = "btn_combine";
+            this.btn_combine.Size = new System.Drawing.Size(75, 23);
+            this.btn_combine.TabIndex = 6;
+            this.btn_combine.Text = "Combine";
+            this.btn_combine.UseVisualStyleBackColor = true;
+            this.btn_combine.Click += new System.EventHandler(this.btn_combine_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "github.com/onursert";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(460, 225);
+            this.listBox1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(174, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(298, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(93, 12);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_refresh.TabIndex = 9;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // PDFCombine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 312);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_add_pdf);
+            this.Controls.Add(this.btn_combine);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "PDFCombine";
+            this.Text = "PDF Combine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add_pdf;
+        private System.Windows.Forms.Button btn_combine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btn_refresh;
+    }
+}
+
---
+++ PDFCombine/PDFCombine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+
+namespace PDFCombine
+{
+    public partial class PDFCombine : Form
+    {
+        public PDFCombine()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_pdf_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+
+        private void btn_combine_Click(object sender, EventArgs e)
+        {
+            PdfDocument pdfOutput = new PdfDocument(new PdfWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Output.pdf"));
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                PdfDocument pdfToCombine = new PdfDocument(new PdfReader(listBox1.Items[i].ToString()));
+                PdfMerger merger = new PdfMerger(pdfOutput);
+                merger.Merge(pdfToCombine, 1, pdfToCombine.GetNumberOfPages());
+                pdfToCombine.Close();
+
+                progressBar1.Value = (i + 1) * 100 / listBox1.Items.Count;
+            }
+
+            pdfOutput.Close();
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            progressBar1.Value = 0;
+        }
+    }
+}
